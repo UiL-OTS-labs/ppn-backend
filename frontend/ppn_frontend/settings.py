75d7e1dc81cb079e0cfa530b20005e7449001fc4
ben <b.bonfil@uu.nl>
2025-09-30 16:39:46 +0200

    frontend doesn't really need a database

--- frontend/ppn_frontend/settings.py
+++ frontend/ppn_frontend/settings.py
@@ -163,13 +163,9 @@ WSGI_APPLICATION = 'ppn_frontend.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': int(os.environ.get('DB_PORT', '3306')),
-        'NAME': os.environ.get('DB_NAME', 'ppn_frontend'),
-        'USER': os.environ.get('DB_USER', 'ppn'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'ppn')
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
