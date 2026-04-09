87a18f472ae559535f78d88cad503ba2ad840985
ben <b.bonfil@uu.nl>
2026-04-09 13:50:24 +0200

    changed integration tests settings to not overwrite dev db

--- integration_tests/backend_settings.py
+++ integration_tests/backend_settings.py
@@ -3,5 +3,6 @@ sys.path.append('../backend')
 
 from ppn_backend.settings import *
 
+DATABASES['default']['NAME'] = 'ppn_test'
 FIXTURE_DIRS = ['data_fixtures']
 DATABASES['default']['NAME'] = 'ppn-test'
